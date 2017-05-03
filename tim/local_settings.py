99872e96feb24e50ebad48a47bbb3b1b8ebe2c00
konoufo <konoufo1@gmail.com>
2017-05-03 15:43:41 -0400

    local_settings import

---
+++ tim/local_settings.py
@@ -0,0 +1,14 @@
+"""
+Local settings of current environment
+"""
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_local.sqlite3'),
+    }
+}
--- tim/settings.py
+++ tim/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from tim.local_settings import *
+except:
+    pass
