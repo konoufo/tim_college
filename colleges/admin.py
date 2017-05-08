c9289e3595ae7423be2267acfbadf70b1708b454
konoufo <konoufo1@gmail.com>
2017-05-08 00:13:47 -0400

    added description on models admin interface.

--- colleges/admin.py
+++ colleges/admin.py
@@ -4,11 +4,69 @@ from django.contrib import admin
 from .models import School, Scholarship, StudyField, Career, StudyProgram, FieldCareer, FieldProgram, ProgramTuition
 
 
+class FieldCareerAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+        # here we define a custom template
+        self.change_form_template = 'admin/tim/change_form_help_text.html'
+        extra = {
+            'help_text': """This represents every linked career and field of study.
+                            In the sense that many fields can lead to the same career.
+                            Whereas many careers can span from one field of study."""
+        }
+
+        context.update(extra)
+        return super(FieldCareerAdmin, self).render_change_form(request, context, *args, **kwargs)
+
+
+class FieldProgramAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+        # here we define a custom template
+        self.change_form_template = 'admin/tim/change_form_help_text.html'
+        extra = {
+            'help_text': """This represents every linked school program and field of study.
+                            In the sense that there are many programs in a same field of study.
+                            Whereas a program could span several fields of study."""
+        }
+
+        context.update(extra)
+        return super(FieldProgramAdmin, self).render_change_form(request, context, *args, **kwargs)
+
+
+class StudyProgramAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+        # here we define a custom template
+        self.change_form_template = 'admin/tim/change_form_help_text.html'
+        extra = {
+            'help_text': """This represents an official university program.
+
+                            """
+        }
+
+        context.update(extra)
+        return super(StudyProgramAdmin, self).render_change_form(request, context, *args, **kwargs)
+
+
+class StudyFieldAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+        # here we define a custom template
+        self.change_form_template = 'admin/tim/change_form_help_text.html'
+        extra = {
+            'help_text': """This represents an arbitrary field of study i.e Mathematics, Sociology, or Robotics.
+                            It is NOT an official university program per se, it's the larger area of knowledge
+                            a university program can be taken from. Ex: BSc in Mathematics and CS is part of
+                            the study fields of Mathematics and also of Computing.
+                             """
+        }
+
+        context.update(extra)
+        return super(StudyFieldAdmin, self).render_change_form(request, context, *args, **kwargs)
+
+
 admin.site.register(School)
 admin.site.register(Scholarship)
-admin.site.register(StudyField)
+admin.site.register(StudyField, StudyFieldAdmin)
 admin.site.register(Career)
-admin.site.register(StudyProgram)
-admin.site.register(FieldCareer)
-admin.site.register(FieldProgram)
+admin.site.register(StudyProgram, StudyProgramAdmin)
+admin.site.register(FieldCareer, FieldCareerAdmin)
+admin.site.register(FieldProgram, FieldProgramAdmin)
 admin.site.register(ProgramTuition)
--- colleges/models.py
+++ colleges/models.py
@@ -2,48 +2,53 @@ from django.db import models
 
 
 class School(models.Model):
-    name = models.CharField(max_length=250)
-    type = models.IntegerField(choices=((1, 'university'), (2, 'other')))
-    location = models.IntegerField(choices=((1, 'Lusaka'), (2, 'Ndola'), (3, 'Choma'), (4, 'Chipata'), ))
-    ownership = models.IntegerField(choices=((1, 'private'), (2, 'public'), ))
+    name = models.CharField(max_length=250, help_text='Name of Institution')
+    type = models.IntegerField(choices=((1, 'university'), (2, 'other')), help_text='Type of institution.')
+    location = models.IntegerField(choices=((1, 'Lusaka'), (2, 'Ndola'), (3, 'Choma'), (4, 'Chipata'), ),
+                                   help_text='State location of this school.')
+    ownership = models.IntegerField(choices=((1, 'private'), (2, 'public'), ),
+                                    help_text='Is it a private school or a public school ?')
 
 
 class Scholarship(models.Model):
-    conditions = models.CharField(max_length=999)
-    amount = models.IntegerField()
+    conditions = models.CharField(max_length=999, help_text='List of conditions for this scholarship. (separator: ";")')
+    amount = models.IntegerField(help_text='Amount of money in this scholarship.')
 
 
 class StudyField(models.Model):
-    name = models.CharField(max_length=250)
-    description = models.CharField(max_length=250)
+    name = models.CharField(max_length=250,  help_text='Name of this field of study.')
+    description = models.CharField(max_length=250, help_text='Description of this field of study.')
 
 
 class Career(models.Model):
-    name = models.CharField(max_length=250)
-    description = models.CharField(max_length=500)
+    name = models.CharField(max_length=250, help_text='Name of this career')
+    description = models.CharField(max_length=500, help_text='Description of this career')
 
 
 class StudyProgram(models.Model):
-    name = models.CharField(max_length=250)
-    school = models.ForeignKey(School, related_name='programs')
-    level = models.IntegerField(choices=((1, 'undergraduate'), (2, 'graduate')))
+    name = models.CharField(max_length=250, help_text='Name of this program.')
+    school = models.ForeignKey(School, related_name='programs', help_text='School with this program.')
+    level = models.IntegerField(choices=((1, 'undergraduate'), (2, 'graduate')),
+                                help_text='Level of current program.')
     length = models.IntegerField(help_text='Length of this study program in the selected base unit.')
     base_time_unit = models.IntegerField(choices=((1, 'semester(s)'), (2, 'year(s)')))
 
 
 class FieldCareer(models.Model):
-    career = models.ForeignKey(Career, related_name='fields')
-    field = models.ForeignKey(StudyField, related_name='careers')
+    career = models.ForeignKey(Career, related_name='fields', help_text='Career linked with field of study.')
+    field = models.ForeignKey(StudyField, related_name='careers', help_text='Field of study linked with program.')
 
 
 class FieldProgram(models.Model):
-    program = models.ForeignKey(StudyProgram, related_name='fields')
-    field = models.ForeignKey(StudyField, related_name='programs')
+    program = models.ForeignKey(StudyProgram, related_name='fields', help_text='Program linked with field of study.')
+    field = models.ForeignKey(StudyField, related_name='programs', help_text='Field of study linked with program.')
 
 
 class ProgramTuition(models.Model):
-    program = models.ForeignKey(StudyProgram, related_name='tuitions')
-    student_category = models.CharField(max_length=250)
-    period = models.CharField(max_length=25)
+    program = models.ForeignKey(StudyProgram, related_name='tuitions',
+                                help_text='university program linked to this tuition.')
+    student_category = models.CharField(max_length=250,
+                        help_text='Student category concerned by this tuition (e.g. foreign student, in-state student)')
+    period = models.CharField(max_length=25, help_text='Period for tuition payment (e.g. year, semester)')
     payments = models.CharField(max_length=999, help_text='List of payments asked each period. (separator: ";")')
     total = models.IntegerField(help_text='Total amount to pay for tuition for the entire program.')
