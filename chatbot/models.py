454cf086591148d6dfabe60c47c8349cdabaa1d3
konoufo <konoufo1@gmail.com>
2017-05-12 03:29:34 -0400

    session/conversation logic

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+from importlib import import_module
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
+
+
+class Conversation(models.Model):
+    phone_number = models.CharField(max_length=17)
+    session_id = models.CharField(max_length=9999)
+
+
+def create_session_with_number(phone_number):
+    s = SessionStore()
+    s['phone_number'] = phone_number
+    s.create()
+    return s
+
+def get_session(session_id):
+    return SessionStore(session_key=session_id)
+
+def update_session_with_context(session_id, context):
+    s = get_session(session_id)
+    if session_id is None:
+        s.create()
+    s['context'] = s.get('context', {})
+    s['context'].update(context)
+    s.save()
+    return s
+
+def get_current_conversation(phone_number):
+    try:
+        return Conversation.objects.get(phone_number=phone_number)
+    except Conversation.DoesNotExist:
+        return None
+
+def create_conversation(phone_number, session_id):
+    conv, _ = Conversation.objects.get_or_create(phone_number=phone_number, session_id=session_id)
+    return conv
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,12 +1,16 @@
+import logging
+
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
 
 from django.shortcuts import render
 from django.views.generic.base import View
-from twilio.rest import Client
-from twilio.twiml.messaging_response import MessagingResponse
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import logging
+
 from wit import Wit
+
+from chatbot.models import get_current_conversation, create_conversation, create_session_with_number, get_session, \
+    update_session_with_context
 # from .queries import *
 
 
@@ -27,8 +31,13 @@ access_token = 'HYIAH4DQRFWM65BNI2SNQ2TPAM2ZGV7L'
 # Send function for Wit Actions
 def send_sms(request, response):
     client_twilio = Client(account_sid, auth_token)
+    try:
+        s = get_session(request['session_id'])
+    except KeyError:
+        print('session_id is missing')
+        return None
     message = client_twilio.messages.create(
-        to=user_number,
+        to=s['phone_number'],
         from_="+14387956005",
         body=response['text'])
     logger.error(request.POST.get('From', None))
@@ -38,12 +47,18 @@ def send_sms(request, response):
 @csrf_exempt
 def inbound(request):
     message = request.POST.get('Body', None)
-    global user_number
     user_number = request.POST.get('From', None)
+    conversation = get_current_conversation(user_number)
+    try:
+        s = get_session(conversation.session_id)
+    except AttributeError:
+        s = create_session_with_number(user_number)
+        create_conversation(user_number, s.session_key)
     logger.error(user_number)
-    session_id = 'my-user-session-43'
-    context0 = {}
-    client_wit.run_actions(session_id, message, context0)
+    session_id = s.session_key
+    s['context'] = s.get('context', {})
+    s['context'].update(client_wit.run_actions(session_id, message, s['context']))
+    s.save()
     return None
 
 
@@ -65,12 +80,10 @@ def first_entity_value(entities, entity):
 def start_conversation(request):
     context = request['context']
     entities = request['entities']
-
+    session_id = request['session_id']
     name = first_entity_value(entities, 'contact')
     greetings = first_entity_value(entities, 'greetings')
-
     if greetings:
-
         if name:
             context['name'] = name
             if context.get('noName') is not None:
@@ -79,20 +92,19 @@ def start_conversation(request):
             context['noName'] = True
             if context.get('name') is not None:
                 del context['name']
-
-    return context
+    s = update_session_with_context(session_id, context)
+    return s['context']
 
 
 def filter_by_location(request):
     context = request['context']
     entities = request['entities']
-
+    session_id = request['session_id']
     location = first_entity_value(entities, 'location')
-
     if location:
         context['location'] = location
-
-    return context
+    s = update_session_with_context(session_id, context)
+    return s['context']
 
 actions = {
     'send': send_sms,
