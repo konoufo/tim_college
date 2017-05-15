9bdc0ae68d3a391a8729164e5a8ef16390fb02fa
konoufo <konoufo1@gmail.com>
2017-05-14 22:38:04 -0400

    interactive works

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,8 +1,17 @@
 from importlib import import_module
+import uuid
+
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.contrib.sessions.models import Session
+from django.utils import timezone
 
-SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
+
+try:
+    SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
+except ImproperlyConfigured:
+    SessionStore = import_module('django.contrib.sessions.backends.db').SessionStore
 
 
 class Conversation(models.Model):
@@ -17,19 +26,33 @@ def create_session_with_number(phone_number):
     return s
 
 
-def get_session(session_id):
-    return SessionStore(session_key=session_id)
+def create_session(session_id=None):
+    if session_id is not None and isinstance(session_id, uuid.UUID):
+        s = Session(expire_date=timezone.now() + timezone.timedelta(hours=1))
+        s.pk = str(session_id)
+        s.save()
+        return s
+    s = SessionStore()
+    s.create()
+    return s
 
+def get_session(session_id):
+    if isinstance(session_id, uuid.UUID):
+        session_id = str(session_id)
+    return SessionStore(session_key=session_id)
 
 def update_session_with_context(session_id, context):
     s = get_session(session_id)
-    if session_id is None:
-        s.create()
     s['context'] = s.get('context', {})
     s['context'].update(context)
     s.save()
     return s
 
+def create_session_with_context(context):
+    s = SessionStore()
+    s.create()
+    s['context'].update(context)
+    return s
 
 def get_current_conversation(phone_number):
     try:
--- chatbot/wit_ai_manager.py
+++ chatbot/wit_ai_manager.py
@@ -1,6 +1,25 @@
-from chatbot.models import update_session_with_context, get_session
+import os
+
+from django.conf import settings
+import django
+
 from wit import Wit
 from twilio.rest import Client
+
+if not settings.configured:
+    import os, sys
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(base_dir)
+    # sys.path.append(os.path.join(base_dir, 'tim'))
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tim.settings"
+    print(settings.DEBUG)
+    # Make project directory the current working directory.
+    # os.chdir(base_dir)
+    # This is so models get loaded. I don't know why django.setup() is not working.
+    django.setup()
+    print('setup done')
+from .models import update_session_with_context, get_session, create_session_with_context
+
 
 # Twilio credentials
 account_sid = "AC2753af86e1e282a4ca4a79d40380e923"
@@ -23,6 +42,9 @@ def send_sms(request, response):
         s = get_session(request['session_id'])
     except KeyError:
         print('session_id is missing')
+        return None
+    except IndexError:
+        print('session does not exist')
         return None
     message = client_twilio.messages.create(
         to=s['phone_number'],
@@ -55,7 +77,10 @@ def start_conversation(request):
             context['noName'] = True
             if context.get('name') is not None:
                 del context['name']
-    s = update_session_with_context(session_id, context)
+    try:
+        s = update_session_with_context(session_id, context)
+    except IndexError:
+        s = create_session_with_context(context)
     return s['context']
 
 
@@ -66,7 +91,10 @@ def filter_by_location(request):
     location = first_entity_value(entities, 'location')
     if location:
         context['location'] = location
-    s = update_session_with_context(session_id, context)
+    try:
+        s = update_session_with_context(session_id, context)
+    except IndexError:
+        s = create_session_with_context(context)
     return s['context']
 
 actions = {
@@ -78,4 +106,5 @@ actions = {
 client_wit = Wit(access_token=access_token, actions=actions)
 
 if __name__ == "__main__":
+    actions.update({'send': send})
     client_wit.interactive()
