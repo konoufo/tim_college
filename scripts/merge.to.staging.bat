c089a5c85e8fe50823f61ceacacfea4ad406be8d
konoufo <konoufo1@gmail.com>
2017-07-13 01:12:45 -0400

    created .gitatrributes file

---
+++ scripts/merge.to.staging.bat
@@ -0,0 +1,1 @@
+git checkout staging && git merge master && git reset HEAD */migrations/** && git checkout -f */migrations/**
---
+++ scripts/merge.to.staging.sh
@@ -0,0 +1,2 @@
+git checkout staging
+git merge master && git reset HEAD */migrations/** && git checkout -f */migrations/**
